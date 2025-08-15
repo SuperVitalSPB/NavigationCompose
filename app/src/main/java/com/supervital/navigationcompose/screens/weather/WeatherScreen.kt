454f377286dca8f8c660e98d8acf83a8d9fbd2e8
Виталий Оанца <o_vital@mail.ru>
2025-08-15 11:45:13 +0300

    next iteration

--- app/src/main/java/com/supervital/navigationcompose/screens/weather/WeatherScreen.kt
+++ app/src/main/java/com/supervital/navigationcompose/screens/weather/WeatherScreen.kt
@@ -1,7 +1,6 @@
 package com.supervital.navigationcompose.screens.weather
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -21,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -45,82 +41,75 @@ fun WeatherScreen() {
                 .fillMaxSize()
                 .padding(5.dp)
         ) {
-            Card(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth(),
-                shape = RoundedCornerShape(10.dp)
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(
+                Row(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .background(colorResource(R.color.blue_ligh).copy(alpha = 0.5f)),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Row(
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "14.08.2025",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "im2",
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                            .size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Madrid",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23ºC",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
+
+                        }
                     ) {
-                        Text(
-                            modifier = Modifier.padding(top = 8.dp, start = 8.dp),
-                            text = "14.08.2025",
-                            style = TextStyle(fontSize = 15.sp),
-                            color = Color.White
-                        )
-                        AsyncImage(
-                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
-                            contentDescription = "im2",
-                            modifier = Modifier
-                                .size(35.dp)
-                                .padding(top = 3.dp, end = 8.dp)
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
                         )
                     }
                     Text(
-                        text = "Madrid",
-                        style = TextStyle(fontSize = 24.sp),
-                        color = Color.White
-                    )
-                    Text(
-                        text = "23ºC",
-                        style = TextStyle(fontSize = 65.sp),
-                        color = Color.White
-                    )
-                    Text(
-                        text = "Sunny",
+                        text = "23ºC/12ºC",
                         style = TextStyle(fontSize = 16.sp),
                         color = Color.White
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                    IconButton(
+                        onClick = {
+
+                        }
                     ) {
-                        IconButton(
-                            onClick = {
-
-                            }
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_search),
-                                contentDescription = "im3",
-                                tint = Color.White
-                            )
-                        }
-                        Text(
-                            text = "23ºC/12ºC",
-                            style = TextStyle(fontSize = 16.sp),
-                            color = Color.White
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = Color.White
                         )
-                        IconButton(
-                            onClick = {
-
-                            }
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_sync),
-                                contentDescription = "im4",
-                                tint = Color.White
-                            )
-                        }
                     }
                 }
             }
