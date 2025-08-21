077f4c038b1a22e2997bb167d6fe51f1a254a27b
Виталий Оанца <o_vital@mail.ru>
2025-08-21 08:51:52 +0300

    correct place start screen

--- app/src/main/java/com/supervital/navigationcompose/MainActivity.kt
+++ app/src/main/java/com/supervital/navigationcompose/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.supervital.navigationcompose.screens.weather.MainScreenWithNavigationAndListScreen
+import com.supervital.navigationcompose.screens.MainScreenWithNavigationAndListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/supervital/navigationcompose/screens/weather/WeatherScreen.kt
+++ app/src/main/java/com/supervital/navigationcompose/screens/weather/WeatherScreen.kt
@@ -41,49 +41,6 @@ import com.supervital.navigationcompose.ui.theme.NavigationComposeTheme
 import java.text.SimpleDateFormat
 import java.util.Date
 
-
-@Composable
-fun MainScreenWithNavigationAndListScreen() {
-    var selectedIndex by remember { mutableIntStateOf(0) }
-    val items = NavBarItems.BarItems
-    val selectedBarItem = NavBarItems.BarItems[selectedIndex]
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        icon = {
-                            Icon(
-                                item.image,
-                                contentDescription = item.title
-                            )
-                        },
-                        label = { Text(item.title) },
-                        selected = selectedIndex == index,
-                        onClick = { selectedIndex = index }
-                    )
-                }
-            }
-        }
-    ) { innerPadding ->
-        NavigationComposeTheme {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(all = 5.dp)
-                    .padding(innerPadding) // Apply padding from Scaffold
-            ) {
-                when (selectedBarItem.route) {
-                    NavRoutes.Home.route -> HomeScreen()
-                    NavRoutes.Contacts.route -> ContactsScreen()
-                    NavRoutes.Weather.route -> WeatherScreen()
-                    NavRoutes.About.route -> AboutScreen()
-                }
-            }
-        }
-    }
-}
-
 @Composable
 fun WeatherScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
